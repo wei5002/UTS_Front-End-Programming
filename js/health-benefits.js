7a03e80824e461772b25ab8d9a41dca2c3894521
wei5002 <shirleywei08@gmail.com>
2024-09-20 20:47:16 +0700

    perbaiki navbar health benefit

--- js/health-benefits.js
+++ js/health-benefits.js
@@ -1,11 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
     // Menangani klik pada item navbar
-    document.querySelectorAll('#navbar-data a').forEach(anchor => {
+    document.querySelectorAll('.navbar-data a').forEach(anchor => {
       anchor.addEventListener('click', function(e) {
         e.preventDefault();
         
         // Ambil target ID
-        const targetId = this.getAttribute('href').substring(1);
+        const targetId = this.getAttribute('onclick').match(/'([^']+)'/)[1];
         const targetElement = document.getElementById(targetId);
         
         if (targetElement) {
