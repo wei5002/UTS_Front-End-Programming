07de54001df2e910a3d2bdc8f0265e6a822f802a
Jovandi <162522277+MrJov100@users.noreply.github.com>
2024-09-27 21:03:31 +0700

    fix bug, add feature, renew navbar, etc

--- js/events-script.js
+++ js/events-script.js
@@ -39,3 +39,57 @@ document.addEventListener("DOMContentLoaded", () => {
     darkModeToggle.addEventListener("click", toggleDarkMode);
   }
 });
+
+// Fungsi untuk ambil data dari API
+async function fetchSportsEvents() {
+  try {
+    const response = await fetch("../data/sports_events.json");
+
+    const data = await response.json();
+    const eventsContainer = document.getElementById("events-container");
+
+    data.sportsEvents.forEach((event, index) => {
+      const eventBox = document.createElement("div");
+      eventBox.classList.add("event-box");
+
+      // Tambahkan kelas 'event-box-hot' untuk dua events teratas
+      if (index < 2) {
+        eventBox.classList.add("event-box-hot");
+      }
+
+      const eventBoxInner = document.createElement("div");
+      eventBoxInner.classList.add("event-box-inner");
+
+      // Bagian depan kotak events
+      const eventBoxFront = document.createElement("div");
+      eventBoxFront.classList.add("event-box-front");
+      eventBoxFront.innerHTML = `
+              <img src="../assets/images/sports-logos/${event.sport.toLowerCase()}-logo.jpg" alt="${
+        event.sport
+      } Logo" />
+              <h2>${event.title}</h2>
+            `;
+
+      const eventBoxBack = document.createElement("div");
+      eventBoxBack.classList.add("event-box-back");
+      eventBoxBack.innerHTML = `
+              <div class="event-details">
+                <p><strong>Date:</strong> ${event.date}</p>
+                <p><strong>Location:</strong> ${event.location}</p>
+                <p>${event.description}</p>
+              </div>
+            `;
+
+      eventBoxInner.appendChild(eventBoxFront);
+      eventBoxInner.appendChild(eventBoxBack);
+      eventBox.appendChild(eventBoxInner);
+
+      eventsContainer.appendChild(eventBox);
+    });
+  } catch (error) {
+    console.error("Error fetching sports events:", error);
+  }
+}
+
+// Panggil fungsi acara
+fetchSportsEvents();
