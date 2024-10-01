c04f28f3baed969574d92ea7429df8916309f574
Jovandi <162522277+MrJov100@users.noreply.github.com>
2024-10-01 20:28:54 +0700

    filter bulan pada page upcoming-events

--- js/events-script.js
+++ js/events-script.js
@@ -46,23 +46,45 @@ document.addEventListener("DOMContentLoaded", function () {
     .then((response) => response.json())
     .then((data) => {
       const tableBody = document.getElementById("event-table");
-      let rows = "";
+      const monthFilter = document.getElementById("month-filter");
+      let events = data.events;
 
-      // Loop melalui setiap event dan buat baris tabel dengan data-label untuk mobile view
-      data.events.forEach((event) => {
-        rows += `
-          <tr>
-            <td data-label="Date">${event.date}</td>
-            <td data-label="Event Name">${event.eventName}</td>
-            <td data-label="Category">${event.category}</td>
-            <td data-label="Event Type">${event.eventType}</td>
-            <td data-label="Location">${event.location}</td>
-          </tr>
-        `;
+      // Fungsi untuk memperbarui tabel dengan acara yang sudah difilter
+      const updateTable = (filteredEvents) => {
+        let rows = "";
+        filteredEvents.forEach((event) => {
+          rows += `
+            <tr>
+              <td data-label="Date">${event.date}</td>
+              <td data-label="Event Name">${event.eventName}</td>
+              <td data-label="Category">${event.category}</td>
+              <td data-label="Event Type">${event.eventType}</td>
+              <td data-label="Location">${event.location}</td>
+            </tr>
+          `;
+        });
+        tableBody.innerHTML = rows;
+      };
+      updateTable(events);
+
+      monthFilter.addEventListener("change", function () {
+        const selectedMonth = monthFilter.value;
+        let filteredEvents;
+
+        if (selectedMonth === "all") {
+          filteredEvents = events;
+        } else {
+          filteredEvents = events.filter((event) => {
+            const eventDate = new Date(event.date);
+            return (
+              String(eventDate.getMonth() + 1).padStart(2, "0") ===
+              selectedMonth
+            );
+          });
+        }
+
+        updateTable(filteredEvents);
       });
-
-      // Menambahkan baris ke tabel
-      tableBody.innerHTML = rows;
     })
     .catch((error) => console.error("Error fetching the events:", error));
 });
